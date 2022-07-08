44e6d27e744a567c6034d46c3f2979ad6f21a59d
Jesus Lazo Tapia <u202019038@upc.edu.pe>
2022-07-07 20:55:59 -0500

    Initial commit

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,10 @@
+
+$.get("_header.html",contentToInject => {
+
+    $("#header-placeholder").replaceWith(contentToInject);
+});
+
+$.get("_footer.html",contentToInject => {
+
+    $("#footer-placeholder").replaceWith(contentToInject);
+});
